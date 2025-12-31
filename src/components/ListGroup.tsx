50e2f1c48aea5e8a973a27f440f095d56e2781c0
Shafi Yaftali <shafiyaftali001@gmail.com>
2025-12-31 11:13:14 -0500

    feat: useStae hook the handles the state of selected cities based on the use interaction

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,21 @@
+import { useState } from "react";
+
 const ListGroup = () => {
   const cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedCity, setSelectedCity] = useState<string | null>(null); 
   const getMessage = () => {
     return cities.length === 0 ? <p>No cities found</p> : <p>Cities found</p>;
   };
-  // this is a function that handles the click event
-  // it takes a city as an argument
-  // it logs the city to the console
+
   const handleClick = (city: string) => {
-    console.log(city);
+    setSelectedCity(city);
   };
   const getCities = () => {
     return cities.length > 0 ? (
       <ul className="list-group">
         {cities.map((city) => (
           <li
-            className="list-group-item"
+            className={selectedCity === city ? "list-group-item active" : "list-group-item"}
             key={city}
             onClick={() => handleClick(city)}
           >
