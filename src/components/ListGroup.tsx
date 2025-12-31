123dfaf9116e51be2d2e80593545c00556f08479
Shafi Yaftali <shafiyaftali001@gmail.com>
2025-12-31 09:49:29 -0500

    feat: Created GroupComponent

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,11 @@
+const ListGroup = () => {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">Jhon Doe</li>
+      <li className="list-group-item">Jane Doe</li>
+      <li className="list-group-item">Jim Doe</li>
+    </ul>
+  );
+};
+
+export default ListGroup;
---
+++ src/components/Message.tsx
@@ -0,0 +1,11 @@
+const Message = () => {
+  return (
+    <div className="container mt-5 bg-light p-5 rounded-3 text-center shadow-lg border border-primary mb-5 fw-bold display-6 text-primary fw-bold">
+      <h1 className="text-primary fs-1">Hello World</h1>
+      <p className="text-secondary fs-2">This is a paragraph</p>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+};
+// bootstrap class names and designing the elements
+export default Message;
