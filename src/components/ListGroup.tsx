e2e9844d3210a09dedd9f66aeb728416f6bb51c2
Shafi Yaftali <shafiyaftali001@gmail.com>
2025-12-31 10:48:34 -0500

    feat: listgroup component has a function that handles the client event it takes a city as an argument it logs the city toe the console.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,16 +3,28 @@ const ListGroup = () => {
   const getMessage = () => {
     return cities.length === 0 ? <p>No cities found</p> : <p>Cities found</p>;
   };
+  // this is a function that handles the click event
+  // it takes a city as an argument
+  // it logs the city to the console
+  const handleClick = (city: string) => {
+    console.log(city);
+  };
   const getCities = () => {
     return cities.length > 0 ? (
       <ul className="list-group">
         {cities.map((city) => (
-          <li className="list-group-item" key={city}>
+          <li
+            className="list-group-item"
+            key={city}
+            onClick={() => handleClick(city)}
+          >
             {city}
           </li>
         ))}
       </ul>
-    ) : <p>No cities found</p>;
+    ) : (
+      <p>No cities found</p>
+    );
   };
   return (
     <>
@@ -24,15 +36,3 @@ const ListGroup = () => {
 };
 
 export default ListGroup;
-
-
-// this component is a list group that displays a list of cities
-// it uses the cities array to display the list of cities
-// it uses the getMessage function to display a message if the cities array is empty
-// it uses the getCities function to display a list of cities if the cities array is not empty
-// in react we use the return statement to return the JSX code
-// we use the map function to iterate over the cities array and return a list of cities
-// we use the key prop to uniquely identify each city
-// we use the className prop to apply bootstrap classes to the list group
-// we use the list-group class to apply bootstrap classes to the list group
-// we use the list-group-item class to apply bootstrap classes to the list group items
