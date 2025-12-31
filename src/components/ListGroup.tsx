0fae17e0bac61704636642f8b3d153fa493252aa
Shafi Yaftali <shafiyaftali001@gmail.com>
2025-12-31 10:05:56 -0500

    feat: wrap the elements inside the listGroup component with fragement to make sure react doesn't throw us with error

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,13 @@
 const ListGroup = () => {
   return (
+    <>
+    <h1 className="mb-3">List Group</h1>
     <ul className="list-group">
-      <li className="list-group-item">Jhon Doe</li>
-      <li className="list-group-item">Jane Doe</li>
-      <li className="list-group-item">Jim Doe</li>
-    </ul>
+        <li className="list-group-item">Jhon Doe</li>
+        <li className="list-group-item">Jane Doe</li>
+        <li className="list-group-item">Jim Doe</li>
+      </ul>
+    </>
   );
 };
 
