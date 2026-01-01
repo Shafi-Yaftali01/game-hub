f31895f4600031c3e721ac71c3cc8a91ec330941
Shafi Yaftali <shafiyaftali001@gmail.com>
2026-01-01 13:49:18 -0500

    feat: reuseable form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
+
 const App = () => {
   return (
-    <div className="container mt-5 d-flex justify-content-center align-items-center flex-column gap-4 flex-wrap">
-      <ExpandableText
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        maxLength={100}
-      />
+    <div>
+      <Form />
     </div>
   );
 };
